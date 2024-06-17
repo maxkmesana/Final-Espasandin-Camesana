ff8a49e94af82835bd7d994917eba67ef3544d1e
Max <max.kmesana@gmail.com>
2024-06-17 18:04:44 -0300

    Renombra parametro de search() en intRepository.

--- src/main/java/org/tpfinal/Interfaces/IntRepository.java
+++ src/main/java/org/tpfinal/Interfaces/IntRepository.java
@@ -2,7 +2,7 @@ package org.tpfinal.Interfaces;
 
 public interface IntRepository<T> {
     void add(T add);
-    T search(String username);
+    T search(String searchField);
     void update(T toUpdate, T updated);
     void remove(T remove);
 }
