f4210f6a79cb5845b3e067b3869297315f039b03
Max <max.kmesana@gmail.com>
2024-06-17 18:37:34 -0300

    Implementa primera iteracion de ProductRepository.

--- src/main/java/org/tpfinal/Product/Model/Repository/ProductRepository.java
+++ src/main/java/org/tpfinal/Product/Model/Repository/ProductRepository.java
@@ -1,4 +1,45 @@
 package org.tpfinal.Product.Model.Repository;
 
-public class ProductRepository {
+import org.tpfinal.Interfaces.IntRepository;
+import org.tpfinal.Product.Model.Entity.Product;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ProductRepository implements IntRepository<Product> {
+    private final Set<Product> productSet; // seria final?
+
+    public ProductRepository() {
+        this.productSet = new HashSet<>();
+    } // Se puede cambiar por implementacion de JSON.
+
+    public Set<Product> getProductSet() {
+        return productSet;
+    }
+
+    @Override
+    public void add(Product add) {
+        productSet.add(add);
+    }
+
+    @Override
+    public Product search(String searchField) {
+        for(Product product : productSet){
+            if(product.getName().equals(searchField)){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(Product toUpdate, Product updated) {
+        toUpdate.setTotalPrice(updated.getTotalPrice());
+        toUpdate.setTotalStock(updated.getTotalStock());
+    }
+
+    @Override
+    public void remove(Product remove) {
+        productSet.remove(remove);
+    }
 }
