d48f041b04861a9e79e6537f099ec87172630639
Max <max.kmesana@gmail.com>
2024-06-19 02:22:28 -0300

    Agrega entidad basica a StockFile, sin terminar.

--- src/main/java/org/tpfinal/StockFile/Controller/StockFileController.java
+++ src/main/java/org/tpfinal/StockFile/Controller/StockFileController.java
@@ -1,4 +1,5 @@
 package org.tpfinal.StockFile.Controller;
 
 public class StockFileController {
+
 }
--- src/main/java/org/tpfinal/StockFile/Model/Entity/StockFile.java
+++ src/main/java/org/tpfinal/StockFile/Model/Entity/StockFile.java
@@ -1,5 +1,23 @@
 package org.tpfinal.StockFile.Model.Entity;
 
-public class StockFile {
+import org.tpfinal.Seat.Entity.Seat;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class StockFile {
+    private Date date;
+    private String activity;
+    private Seat purchase;
+    private Seat sales;
+    private List<Seat> balance;
+
+    public StockFile(Date date, String activity, Seat purchase, Seat sales) {
+        this.date = date;
+        this.activity = activity;
+        this.purchase = purchase;
+        this.sales = sales;
+        this.balance = new ArrayList<>();
+    }
 }
