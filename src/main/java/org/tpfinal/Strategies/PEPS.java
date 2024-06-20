17a1d22e6d49003dce1df4af6ab4f2b82cf57e60
Max <max.kmesana@gmail.com>
2024-06-20 01:36:20 -0300

    Agrega implementaciones terminadas de PEPS y UEPS. Comienza implementacion de PPP.

--- src/main/java/org/tpfinal/Strategies/PEPS.java
+++ src/main/java/org/tpfinal/Strategies/PEPS.java
@@ -30,8 +30,21 @@ public class PEPS implements IntStrategy {
 
     /**
      * PEPS (FIFO) implementation.
+     * Contains three base cases: <p>
+     * <code>Case 1:</code> where sale amount is the same as the seat's amount,
+     * the seat will be removed from <code>balance</code> list and added as a
+     * new entity to the return list.<p>
+     *
+     * <code>Case 2:</code> where sale amount is lesser than the Seat's amount,
+     * the Seat's attribute will be set to the new corresponding value and added
+     * as a new entity to the return list.<p>
+     *
+     * <code>Case 3:</code> where sale amount is greater than the Seat's amount,
+     * the Seat's amount (insufficient) is subtracted from sale amount and Sear
+     * is removed from the original list and added to the return list.
      *
      * @param balance list of balances to delete from.
+     * @return a new list containing all the Seat movements made to the original list.
      */
     @Override
     public List<Seat> delete(List<Seat> balance, Integer saleAmount) {
@@ -43,7 +56,9 @@ public class PEPS implements IntStrategy {
             Seat currentSeat = iterator.next();
 
             if(saleAmount.equals(currentSeat.getAmount())){
+                // Adds Seat to return list
                 seatReturn.add(new Seat(currentSeat.getAmount(), currentSeat.getUnitCost()));
+                // Deletes currentSeat because its amount should now be zero.
                 iterator.remove();
                 break;
             }
--- src/main/java/org/tpfinal/Strategies/PPP.java
+++ src/main/java/org/tpfinal/Strategies/PPP.java
@@ -1,4 +1,36 @@
 package org.tpfinal.Strategies;
 
-public class PPP {
+import org.tpfinal.Interfaces.IntRepository;
+import org.tpfinal.Interfaces.IntStrategy;
+import org.tpfinal.Seat.Entity.Seat;
+
+import java.util.List;
+import java.util.UUID;
+
+public class PPP implements IntStrategy {
+
+    @Override
+    public void add(List<Seat> balance, Seat add) {
+        Seat first = balance.getFirst();
+        if(first == null){
+            balance.add(add);
+        }else{
+
+        }
+    }
+
+    @Override
+    public Seat search(List<Seat> balance, UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public void update(Seat toUpdate, Seat updated) {
+
+    }
+
+    @Override
+    public List<Seat> delete(List<Seat> balance, Integer saleAmount) {
+
+    }
 }
