632028d532b7c90eb86c7fa1648d4b16de47f01a
Max <max.kmesana@gmail.com>
2024-06-20 02:24:19 -0300

    Agrega DivisionByZeroException, continua desarrollo de PPP.

--- src/main/java/org/tpfinal/Strategies/PPP.java
+++ src/main/java/org/tpfinal/Strategies/PPP.java
@@ -1,5 +1,6 @@
 package org.tpfinal.Strategies;
 
+import org.tpfinal.Exception.DivisionByZeroException;
 import org.tpfinal.Interfaces.IntRepository;
 import org.tpfinal.Interfaces.IntStrategy;
 import org.tpfinal.Seat.Entity.Seat;
@@ -9,13 +10,19 @@ import java.util.UUID;
 
 public class PPP implements IntStrategy {
 
+    // TODO> throws DivisionByZeroException
     @Override
-    public void add(List<Seat> balance, Seat add) {
+    public void add(List<Seat> balance, Seat add) throws DivisionByZeroException {
         Seat first = balance.getFirst();
         if(first == null){
             balance.add(add);
         }else{
+            first.setAmount(first.getAmount() + add.getAmount());
 
+            if (first.getAmount() == 0){
+                throw new DivisionByZeroException();
+            }
+            first.
         }
     }
 
