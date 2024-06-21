ffd22c24fec0743edce8d0dbf5f212de1053f019
Max <max.kmesana@gmail.com>
2024-06-20 21:01:00 -0300

    Agrega StockFileRepository (sujeto a cambios a futuro).

--- src/main/java/org/tpfinal/StockFile/Model/Repository/StockFileRepository.java
+++ src/main/java/org/tpfinal/StockFile/Model/Repository/StockFileRepository.java
@@ -1,4 +1,44 @@
 package org.tpfinal.StockFile.Model.Repository;
 
-public class StockFileRepository {
+import org.tpfinal.Interfaces.IntRepository;
+import org.tpfinal.StockFile.Model.Entity.StockFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockFileRepository implements IntRepository<StockFile> {
+    private final List<StockFile> stockFileList;
+
+    public StockFileRepository() {
+        this.stockFileList = new ArrayList<>();
+    }
+
+    @Override
+    public void add(StockFile add) {
+        stockFileList.add(add);
+    }
+
+    // TODO: REFACTORIZAR EL SEARCH (String --> UUID en repo GENERICO)
+    @Override
+    public StockFile search(String searchField) {
+        for(StockFile stockFile : stockFileList){
+            if(stockFile.getActivity().equals(searchField)){ // TODO: CAMBIAR POR UUID
+                return stockFile;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(StockFile toUpdate, StockFile updated) {
+        toUpdate.setActivity(updated.getActivity());
+        toUpdate.setDate(updated.getDate());
+        toUpdate.setSale(updated.getSale());
+        toUpdate.setPurchase(updated.getPurchase());
+    }
+
+    @Override
+    public void remove(StockFile remove) {
+        stockFileList.remove(remove);
+    }
 }
