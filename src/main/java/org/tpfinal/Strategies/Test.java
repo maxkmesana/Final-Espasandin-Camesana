e4f454d4d131741dfefbe0df82d3f2d7efa26796
Max <max.kmesana@gmail.com>
2024-06-20 21:50:41 -0300

    Comienza desarrollo de View/Controller de StockFile.

--- src/main/java/org/tpfinal/Strategies/Test.java
+++ src/main/java/org/tpfinal/Strategies/Test.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
+    // TODO: DELETE THIS ENTIRE FILE
     public static void main(String[] args) {
         UEPS ueps = new UEPS();
         ArrayList<Seat> list = new ArrayList<>();
