e71c2bf08fa0960a56d6bcd64403a06c1c66d855
Yatel Espasandin <yatel.espasandin@gmail.com>
2024-06-23 20:56:22 -0300

    Set name Vaguard PIM

--- src/main/java/org/tpfinal/Main.java
+++ src/main/java/org/tpfinal/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("userLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Welcome to StockManager");
+        stage.setTitle("Welcome to Vanguard P.I.M.");
         stage.setScene(scene);
         stage.setResizable(false);
 
